a5c3c173712851d0c11fdfd5062f33f94a99f57b
evelyntys <evelyn.tan@trentglobal.edu.sg>
2022-08-20 14:25:51 +0000

    added navbar and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route, Link, NavLink } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Cart from './pages/Cart';
+import Profile from './pages/Profile';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import Orders from './pages/Orders';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand className="me-auto" as={NavLink} to="/">
+                <img src={require("./images/logo.png")} style={{ "height": "40px" }} />
+                FIGU屋
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ms-auto">
+                  <Nav.Link as={NavLink} to="/products">Products</Nav.Link>
+                  <Nav.Link as={NavLink} to="/cart">Cart</Nav.Link>
+                  <NavDropdown title="User" id="collasible-nav-dropdown">
+                    <NavDropdown.Item as={NavLink} to="/profile">Profile</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/orders">
+                      Orders
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/orders" element={<Orders/>} />
+          </Routes>
+        </Router>
+
+      </React.Fragment>
+    )
+  }
 }
-
-export default App;
---
+++ src/pages/Orders.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Orders(){
+    return (
+        <React.Fragment>
+            Orders
+        </React.Fragment>
+    )
+}

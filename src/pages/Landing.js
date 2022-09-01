9a76f1261915ea6edbaa6231c139768611c46a2c
evelyntys <evelyn.tan@trentglobal.edu.sg>
2022-09-01 05:37:03 +0000

    added styling of landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import ProductContext from '../context/ProductContext';
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function Landing() {
     const navigate = useNavigate();
@@ -17,21 +18,28 @@ export default function Landing() {
         await productContext.getCompleteFigures();
         navigate("/products")
     };
-    
+
     const scale = async () => {
         await productContext.getScaleFigures();
         navigate("/products")
     };
-    
+
     const blind = async () => {
         await productContext.getBlindBox();
         navigate("/products")
     }
 
     const productContext = React.useContext(ProductContext);
+
+    // const [index, setIndex] = React.useState(0);
+
+    // const handleSelect = (selectedIndex, e) => {
+    //     setIndex(selectedIndex);
+    // }
+
     return (
         <React.Fragment>
-            <div className="d-none d-lg-block">
+            <div className="mob-content">
                 <div className="header-wrapper">
                     <video className="header-banner" loop muted autoPlay>
                         <source src={require('../images/header.mp4')} type="video/mp4" />
@@ -41,22 +49,90 @@ export default function Landing() {
                         <button className="btn header-btn" onClick={products}>Shop now</button>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-3 landing-box">
-                        Action Figures <br />
-                        A poseable character model figure, which may have movable figure parts
+                <Carousel>
+                    <Carousel.Item interval={10000}>
+                        <img
+                            className="d-block w-100 caro-overlay"
+                            src={require('../images/action.jpg')}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Action Figures</h3>
+                            <p className="caro-p"> A poseable character model figure, which may have movable figure parts.</p>
+                            <button className="btn header-btn" onClick={action}>See all action figures</button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={10000}>
+                        <img
+                            className="d-block w-100 caro-overlay"
+                            src={require('../images/complete.jpeg')}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Complete Figures</h3>
+                            <p className="caro-p">Typically fixed in position and unmoveable figure parts.</p>
+                            <button className="btn header-btn" onClick={complete}>See all complete figures</button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={10000}>
+                        <img
+                            className="d-block w-100 caro-overlay"
+                            src={require('../images/scale.jpg')}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Scale Figures</h3>
+                            <p className="caro-p">
+                                Fixed-pose figures that are sized based on the “actual” dimensions of the character, with a size reference.
+                            </p>
+                            <button className="btn header-btn" onClick={scale}>See all scale figures</button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={10000}>
+                        <img
+                            className="d-block w-100 caro-overlay"
+                            src={require('../images/blind_box.jpg')}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Blind boxes</h3>
+                            <p className="caro-p">
+                                Collectible toy is kept as a mystery until it is opened.<br/>
+                                Some figurines are rarer to find than others, these are referred to as "secret", "hidden" or "chase" figures.
+                            </p>
+                            <button className="btn caro-btn" onClick={blind}>Test your luck today!</button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+            {/* <div className="d-none d-lg-block">
+                <div className="header-wrapper">
+                    <video className="header-banner" loop muted autoPlay>
+                        <source src={require('../images/header.mp4')} type="video/mp4" />
+                    </video>
+                    <div className="overlay">
+                        Use FREESHIPPING for free standard shipping on purchases over $100 <br />
+                        <button className="btn header-btn" onClick={products}>Shop now</button>
                     </div>
-                    <div className="col-3 landing-box">
-                        Complete Figures <br />
-                        Typically fixed in position and unmoveable figure parts
-                    </div>
-                    <div className="col-3 landing-box">
-                        Scale Figures <br />
-                        Fixed-pose figures that are sized based on the “actual” dimensions of the character, with a size reference
-                    </div>
-                    <div className="col-3 landing-box">
-                        Blind boxes<br />
-                        Blind Box refers to the type of packaging that keeps the collectible toy as a mystery until it is opened. Some figurines are rarer to find than others, these are referred to as "secret", "hidden" or "chase" figures.
+                </div>
+                <div className="">
+                    <div className="row justify-content-center">
+                        <div className="col-10 m-1 landing-box">
+                            Action Figures <br />
+                            A poseable character model figure, which may have movable figure parts
+                        </div>
+                        <div className="col-10 m-1 landing-box">
+                            Complete Figures <br />
+                            Typically fixed in position and unmoveable figure parts
+                        </div>
+                        <div className="col-10 m-1 landing-box">
+                            Scale Figures <br />
+                            Fixed-pose figures that are sized based on the “actual” dimensions of the character, with a size reference
+                        </div>
+                        <div className="col-10 m-1 landing-box">
+                            Blind boxes<br />
+                            Blind Box refers to the type of packaging that keeps the collectible toy as a mystery until it is opened. Some figurines are rarer to find than others, these are referred to as "secret", "hidden" or "chase" figures.
+                        </div>
                     </div>
                 </div>
             </div>
@@ -92,25 +168,7 @@ export default function Landing() {
                         </div>
                     </div>
                 </div>
-                {/* <div className="row">
-                    <div className="col-6 landing-box">
-                        Action Figures <br />
-                        A poseable character model figure, which may have movable figure parts
-                    </div>
-                    <div className="col-6 landing-box">
-                        Complete Figures <br />
-                        Typically fixed in position and unmoveable figure parts
-                    </div>
-                    <div className="col-6 landing-box">
-                        Scale Figures <br />
-                        Fixed-pose figures that are sized based on the “actual” dimensions of the character, with a size reference
-                    </div>
-                    <div className="col-6 landing-box">
-                        Blind boxes<br />
-                        Blind Box refers to the type of packaging that keeps the collectible toy as a mystery until it is opened. Some figurines are rarer to find than others, these are referred to as "secret", "hidden" or "chase" figures.
-                    </div>
-                </div> */}
-            </div>
+            </div> */}
         </React.Fragment>
     )
 }
